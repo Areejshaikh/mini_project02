7ca7fe36bb0b243d26851290ef81e512e18b757f
Areejshaikh <160318564+Areejshaikh@users.noreply.github.com>
2024-04-12 00:25:20 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+"use strict";
+// Q3 Write a program to determine the greater of two numbers.
+let num1 = 99;
+let num2 = 59;
+if (num1 >= num2) {
+    console.log(`${num1} is greater then to ${num2}! `);
+}
+else {
+    console.log(`${num1} is Less then to ${num2} !`);
+}
---
+++ index.ts
@@ -0,0 +1,11 @@
+// Q3 Write a program to determine the greater of two numbers.
+
+
+let num1 : number = 99;
+let num2 : number = 59;
+
+if (num1 >= num2) {
+    console.log(`${num1} is greater then to ${num2}! `);
+} else {
+    console.log(`${num1} is Less then to ${num2} !`);
+}
